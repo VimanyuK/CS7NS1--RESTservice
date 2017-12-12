9583f7fb57cf9ff009ca497b9c7a949171853f08
vimanyu <vimanyukachroo@yahoo.com>
2017-12-12 06:03:32 +0000

    updates for the Master class.
    Trying to implement cyclomatic complexity, few errors still remain. 
    To be removed in the next update.

--- Master.py
+++ Master.py
@@ -45,6 +45,7 @@ class master():
         # Total number of commits in reporepository
         self.total_number_of_commits = len(self.commit_list)  
         print("Number of commits: {}".format(self.total_number_of_commits))
+        self.list_of_cc = []
 
 class fetchRepository(Resource):
     def __init__(self):
@@ -56,11 +57,11 @@ class fetchRepository(Resource):
         self.reqparser = reqparse.RequestParser()
         
         # For every value coming in JSON, you need an argument
-        self.reqparser.add_argument('pullStatus', type=int, location = 'json')
+        self.reqparser.add_argument('pull_status', type=int, location = 'json')
         self.reqparser.add_argument('complexity', type=float, location='json')
         
     def get(self):
-        repo_args = self.reqparser.parse.args()
+        repo_args = self.reqparser.parse_args()
         # check if the repository has been pulled
         if repo_args['pull_status'] == False:
             print('not pulled yet') # for debugging will be removed
@@ -75,7 +76,51 @@ class fetchRepository(Resource):
             print("WORKER NUMBER: {}".format(self.server.current_workers))
 
 api.add_resource(fetchRepository, "/repo", endpoint="repo")
+
+class cyclomatic_complexity(Resource):
+    def __init__(self):
+        global Master
+        # initializing Master server globaly
+        self.server = Master
+        # initializing Resource CLass as per Method Resolution Order
+        super(cyclomatic_complexity, self).__init__()
+        self.reqparser = reqparse.RequestParser()
+        #Arguments for the Json data
+        self.reqparser.add_argument('commitSha', type=str, location = 'json') 
+        self.reqparser.add_argument('complexity', type=float, location='json')
+        
+    def get(self):
+        if self.server.current_workers < self.server.number_of_workers:
+            return {'sha': -2}
+        if len(self.server.commit_list) == 0:
+            return {'sha': -1}
+        
+        commit_value = self.server.commit_list[0]
+        del self.server.commit_list[0]
+        return {'sha':commit_value}
     
+    def post(self):
+        args = self.reqparser.parse_args()
+        print("Received sha {}".format(args['commitSha']))
+        print("Received complexity {}".format(args['complexity']))
+        self.server.list_of_cc.append({'sha':args['commitSha'], 'complexity':args['complexity']})
+        print(self.server.list_of_cc)
+        print('/n', self.server.commit_list)
+        if len(self.server.list_of_cc) == self.server.total_number_of_commits:
+            end_time = time.time() - self.server.start_time
+            print("total time taken {} seconds".format(end_time))
+            total_mean_cc = 0
+            for i in self.server.list_of_cc:
+                if i['complexity'] > 0:
+                    total_mean_cc = total_mean_cc + i['complexity']
+                else:
+                    print("Commit {} has no computable files".format(i['sha']))
+                total_mean_cc = ((total_mean_cc) / (len(self.server.list_of_cc)))
+                print("OVERALL CYCLOMATIC COMPLEXITY FOR REPOSITORY: {}".format(total_mean_cc))
+            return {'success': True}
+        
+api.add_resource(cyclomatic_complexity, "/cyclomatic", endpoint="cyclomatic")
+            
 if __name__ == "__main__":
     Master = master()
     flask.run(port = 5555)
